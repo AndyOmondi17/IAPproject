95ccaf30ed5775baf2672bc02803d43c0d646568
AndyOmondi17 <andyomondi17@gmail.com>
2020-12-07 12:17:36 +0300

    add login form

---
+++ resources/views/header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">E-commerce</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Orders</a></li>
+    </ul>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Cart</a></li>
+    </ul>
+  </div>
+</nav>
+
