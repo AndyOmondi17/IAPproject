22f6aa3ad93d267c9280f1b9cd5f584df748aab3
AndyOmondi17 <andyomondi17@gmail.com>
2020-12-20 14:45:53 +0300

    add cart functionality

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -12,7 +12,13 @@
             <h5>Price:{{$product['price']}}</h5>
             <h5>Category:{{$product['category']}}</h5>
             <h5>Description:{{$product['description']}}</h5>
-       <button class="btn btn-primary">Add to Cart</button>
+        <br><br>
+        <form action="/add_to_cart" method="POST">
+            @csrf
+            <input type="hidden" name="product_id" value="{{$product['id']}}">
+            <button class="btn btn-primary">Add to Cart</button>
+        </form>
+       
        <br><br>
        <button class="btn btn-primary">Buy Now</button>
         <br><br>
